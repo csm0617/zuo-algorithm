161f5a7d88a413b2c5b3e1cb30f8ec799eb1c386
‘陈水明’ <439752552@qq.com>
2023-12-26 17:22:53 +0800

    初始化项目

---
+++ src/main/java/com/csm/zuo/ZuoApplication.java
@@ -0,0 +1,13 @@
+package com.csm.zuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csm/zuo/ZuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csm.zuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
